0ead218a49dcd730e17b3794122296df9101acf6
Juande Santender Vela <juandesant@gmail.com>
2013-06-28 18:43:31 -0400

    Added support for showing only differences in fields that are different, and show both values for table1 and table2.

--- tablecompare.py
+++ tablecompare.py
@@ -16,6 +16,9 @@ keyFieldName = "FIELD1"       # Change to define the display field for row diffe
 def first(x):
     return x[0]
 
+def is_none(x):
+    return type(x) == type(None)
+
 print "MySQL user: "
 username = raw_input()
 
@@ -46,12 +49,39 @@ for table in tableList:
         parameterDict['keyColumn'] = 0 # we try with the first
     tableParameterDict[table] = parameterDict
 
-# Start analysing
-if tableParameterDict[table_1]['fields'] != tableParameterDict[table_2]['fields']:
+# Temporary list with as many "Nulls" as fields in table_1
+fields1, fields2 = (
+    tableParameterDict[table_1]['fields'],
+    tableParameterDict[table_2]['fields']
+)
+tempTableFields = [None] * len(fields1)
+
+# Sort fields in second table in the same way as in the first
+for field in fields1:
+    try:
+        i1 = fields1.index(field)
+        i2 = fields2.index(field)
+        tempTableFields[i1] = fields2[i2]
+    except ValueError:
+        # This means field was not found in table_2
+        print("Field %s not found in table %s" % (field,table_2))
+        continue
+
+# Check for missing fields (there are None entries in tempTableFields)
+if len(filter(is_none, tempTableFields)) > 0:
     print("Different fields")
     for table in tableList:
         print("Fields in table %s:" % table)
         pprint(tableParameterDict[table]['fields'])
+
+# Check if we can detect the key field; if not, we try the first field
+for table in tableList:
+    tableParameterDict[table]['fields'] = tempTableFields
+    try:
+        tableParameterDict[table]['keyColumn'] = tableParameterDict[table]['fields'].index(keyFieldName)
+    except ValueError:
+        tableParameterDict[table]['keyColumn'] = 0 # we try with the first
+
 
 for row in tableParameterDict[table_1]['data']:
     keyFieldValue = row[tableParameterDict[table_1]['keyColumn']]
@@ -94,7 +124,7 @@ for row in tableParameterDict[table_1]['data']:
                 i = diffCols[-1] + 1 # Last index
             except ValueError:
                 break
-        print ("\nDifference found on row %(keyFieldValue)s" % vars())
+        print ("\nDifference found for field %(keyFieldName)s: %(keyFieldValue)s" % vars())
         for col in diffCols:
             print "Field %30s:  value1: %10s     value2: %10s" % (
                 tableParameterDict[table_1]['fields'][col],
